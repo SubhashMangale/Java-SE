8cd19f5ecba104e171b66b0d3103323dae6238f3
Subhash Mangale <58906689+SubhashMangale@users.noreply.github.com>
2022-08-16 14:16:32 +0530

    Create 4.Inheritence.java

---
+++ Basic Java Concepts/Java OOP Programs/4.Inheritence.java
@@ -0,0 +1,35 @@
+/*
+INHERITENCE:
+1. In software development , it is better to use existing resources/code rather than creating all over again. 
+2. This is done using inheritence property which provides outstanding reusability of existing code and which is very imp feature of OOP.
+3. Inheritence provides us a way using which we can use existing code's property in new code.
+4. e.g. Android version 1.0,2.0,3.0 => when they developed android version 2.0 they inherited properties of 1.0 in that and developed new features in 2.0 that they 
+required in 2.0 .
+
+
+Types of Inheritence:
+1. Simple inheritance:  class A => class B                           ------------> One base class and One derived class.
+                                                        class A
+                                                           ||
+                                                        class B     
+                                                       
+
+2. Multilevel Inheritance : class A => class B => class C => class D ------------> One base class -> next derived act as base for its next.
+                                                        class A
+                                                           ||
+                                                        class B
+                                                           ||
+                                                        class C
+                                                        
+
+3. Hierarchical Inheritance: class A => (class A, class B, class C)  ------------> One base class and multiple derived class.
+                                                        class A
+                                                 ||        ||       ||
+                                              class B   class C   class D
+                                              
+
+4. Hybrid Inheritance : Multiple Inheritance + Multilevel Inheritance.
+
+5. Multiple Inheritance: In Java this inheritance is not applicable but concept of that is implemented using INTERFACES.
+
+*/
