e183bdce8fd4ce643bfd64c4f19de0a44804a110
Subhash Mangale <58906689+SubhashMangale@users.noreply.github.com>
2022-08-22 12:49:39 +0530

    Update Stringbuffer class.java

--- Basic Java Concepts/Java OOP Programs/Stringbuffer class.java
+++ Basic Java Concepts/Java OOP Programs/Stringbuffer class.java
@@ -11,3 +11,28 @@ Some methods used in it:
 3.insert() : string1.insert(index_of_string1, string2) :    It will add some new string in between one string. string1.insert(index_of_string1, string2)
 4.setLength():                                         :    Set new length to certain string with forward spaces.
 */
+
+
+// 1.setCharAt(index_of_string1,new_character to insert) :    To replace character at certain index in string and set new character.
+import java.util.Scanner;
+class str{
+	StringBuffer s = new StringBuffer("Margott");
+	void set_char()
+	{
+		s.setCharAt(0, 'k');
+	}
+	void print()
+	{
+		System.out.println("string: "+s);
+	}
+	
+}
+public class array {
+	public static void main(String[] args)
+	{
+		
+		str obj = new str();
+		obj.set_char();
+		obj.print();
+	}
+}
