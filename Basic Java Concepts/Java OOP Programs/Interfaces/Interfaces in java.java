76ac5d621c2d5bffc13994d58fc0cb2b0915a0f3
Subhash Mangale <58906689+SubhashMangale@users.noreply.github.com>
2022-08-25 17:01:15 +0530

    Create Interfaces in java.java

---
+++ Basic Java Concepts/Java OOP Programs/Interfaces/Interfaces in java.java
@@ -0,0 +1,12 @@
+/*
+Interfaces:
+A interfaces is kind of class.It is class like structure which contains only Abstract methods and final fields.
+
+1. Abstract Methods:Abstract methods/functions are such types of functions whicha do not contains curly braces or we can say that which do not implemented.
+	e.g :void function();
+  
+2.final fields: I know that. once we declared one method as final we cannot declare method with same name in further program.	once we declare varible with final keyword 
+then we cannot change it's value.
+
+IMP Note: Concept of Multiple Inheritance can be implemented using Interface in java but it is not present in java directly.
+*/
